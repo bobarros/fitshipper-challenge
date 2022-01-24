78c4419793e60713a6760d2818d9139875b974ba
Bruno Barros <bobarros@gmail.com>
2022-01-24 11:14:59 -0300

    :zap: insert logout button on settings page

--- src/routes/Settings.tsx
+++ src/routes/Settings.tsx
@@ -1,4 +1,9 @@
+// Third Parties
+import { useNavigate } from "react-router-dom";
+import { supabase } from "auth/supabaseClient";
+
 // Types
+import { PrimaryButton } from "components/shared";
 import type { FC } from "react";
 
 /*--------------------*/
@@ -11,9 +16,23 @@ interface IProps {}
  */
 
 const Settings: FC<IProps> = () => {
+  const navigate = useNavigate();
+  // Logoff function
+  const authSignout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log(error);
+    }
+    if (!error) {
+      navigate("/");
+    }
+  };
   return (
-    <div className="h-full">
-      <p className="text-4xl mt-16 ml-8 max-w-md">Settings</p>
+    <div className="h-full pt-16 pl-8 ">
+      <p className="text-4xl max-w-md">Settings</p>
+      <div className="w-32 mt-8 cursor-pointer" onClick={() => authSignout()}>
+        <PrimaryButton>Logout</PrimaryButton>
+      </div>
     </div>
   );
 };
