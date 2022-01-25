15919b1d83ac0ce6a4e8be05c0988d766c01dd31
Bruno Barros <bobarros@gmail.com>
2022-01-25 08:38:16 -0300

    :art: update home for login session

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import type { FC } from "react";
 import { Session } from "@supabase/supabase-js";
 
+// Shared Components
+import { SideBar } from "components/shared";
+
 /*--------------------*/
 
 // Local Types
@@ -31,10 +34,13 @@ const CustomCta = () => {
  */
 const Home: FC<IProps> = ({ session }) => {
   return (
-    <div className="h-full text-center">
-      <p className="text-4xl mt-32 max-w-md mx-auto text-stone-900 dark:text-stone-50">
-        Welcome to the challenge landing page!
-      </p>
+    <div className={`h-full ${session && "grid grid-cols-8"}`}>
+      <SideBar />
+      <div className="p-6 col-span-7">
+        <p className="text-4xl mt-32 max-w-md mx-auto text-stone-900 dark:text-stone-50 text-center ">
+          Welcome to the challenge landing page!
+        </p>
+      </div>
       {session ? null : <CustomCta />}
     </div>
   );
